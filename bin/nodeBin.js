c63a3a1cc92c576c0b8ca298ada1978fa1e2c09a
RobinYan <robin.yan@airwallex.com>
2018-11-12 15:35:53 +0800

    0.1.1

--- bin/nodeBin.js
+++ bin/nodeBin.js
@@ -10,26 +10,10 @@ const { version } = require('../package.json');
 
 yargs
   .version(version)
-  // .command('dev [filename]', 'run the server', (_yargs) => {
-  //   _yargs
-  //     .positional('filename', {
-  //       describe: 'dev: startup file',
-  //       type: 'string'
-  //     })
-  //     .version(false);
-  // }, () => {
-  //   require('../lib/startUp/nodemon');
-  // })
   .command('dev', 'Run the dev server', (argv) => {
     require('../lib/nodemon')(argv);
   })
-  // .command('start', 'start the server', (argv) => {
-  //   // const cmd = `node ${path.resolve(__dirname, '../lib/startUp/server')}`;
-  //   // exec(cmd, (err, stdout, stderr) => {
-  //   //   console.log('err', err);
-  //   //   console.log('stdout', stdout);
-  //   //   console.log('stderr', stderr);
-  //   // });
-  //   require('../lib/startUp/server');
-  // })
+  .command('start', 'Start the server', (argv) => {
+    require('../lib/start')(argv);
+  })
   .argv;
