2eda012898175195bf5fdcd749ab0aab7459a162
RobinYan <robin.yan@airwallex.com>
2019-02-11 14:19:55 +0800

    :sparkles: add docker compose control while run local or debug

--- test/test.js
+++ test/test.js
@@ -1,8 +1,21 @@
-const findUp = require('find-up')
- 
-const test = async () => {
-    const path = findUp.sync('.vscode')
-    console.log(path)
-}
+const yaml = require('js-yaml')
+const fs   = require('fs')
+const {exec} = require('shelljs')
 
-test()
+// let neededPorts = []
+// let port = []
+// try {
+//     var {services} = yaml.safeLoad(fs.readFileSync('/Users/robin.yan/project/airwallex-airboard-ng/service/build/docker-compose.yml', 'utf8'));
+//     for(let key in services) {
+//         port = port.concat(services[key].ports || [])
+//       }
+//       neededPorts = port.map((p) => p.split(':')[0]).filter(p => p !== '0')
+//       console.log(neededPorts)
+// } catch (e) {
+//     console.log(e);
+// }
+exec('lsof -i:8080', function(code, stdout, stderr) {
+    console.log('Exit code:', code);
+    console.log('Program output:', stdout);
+    console.log('Program stderr:', stderr);
+  })
