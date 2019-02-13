40b8d45ace359a482aca27995f36e8bb2b794e4b
RobinYan <robin.yan@airwallex.com>
2019-02-13 15:49:58 +0800

    :sparkles: add docker script

--- bin/nodeBin.js
+++ bin/nodeBin.js
@@ -15,7 +15,13 @@ yargs
     require('../lib/binScript/debug')(argv)
   })
   .command('start', 'Start the server', (argv) => {
-    require('../lib/binScript/start')(argv);
+    require('../lib/binScript/start')(argv)
+  })
+  .command('docker-start', 'Start local docker containers via docker-compose.yml', (argv) => {
+    require('../lib/binScript/docker').start(argv)
+  })
+  .command('docker-stop', 'Stop local docker containers listed in docker-compose.yml', (argv) => {
+    require('../lib/binScript/docker').stop(argv)
   })
   // .command('featureDev', 'debug node server for feature developer', (argv) => {
   //   require('../lib/featureDev')(argv);
